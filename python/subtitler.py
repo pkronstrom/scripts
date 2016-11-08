f41aa3ec96232cb7ee3d76202339bad9b03ee386
Peter Kronström <peter.kronstrom@gmail.com>
2016-11-08 18:09:18 +0200

    Fix intendation

--- python/subtitler.py
+++ python/subtitler.py
@@ -1,17 +1,17 @@
 """
-	This is a crude script to download the first subtitle query from SubScene.
+    This is a crude script to download the first subtitle query from SubScene.
 
-	Install:
-		- python 2.7+ required
-		- pip install beautifulsoup4
+    Install:
+        - python 2.7+ required
+        - pip install beautifulsoup4
 
-	How to use:
-	1) copy the filename to clipboard
-		- Windows: select the file, press F2, CTRL+C
-	2) run the python file (doubleclicking should work fine, if python is in PATH)
-	3) the subtitle file should be downloaded and unzipped to execution folder
+    How to use:
+    1) copy the filename to clipboard
+        - Windows: select the file, press F2, CTRL+C
+    2) run the python file (doubleclicking should work fine, if python is in PATH)
+    3) the subtitle file should be downloaded and unzipped to execution folder
 
-	@author Bembu
+    @author Bembu
 """
 
 from StringIO import StringIO
@@ -29,14 +29,14 @@ query = ""
 ctr = 0
 folder_files = next(os.walk("."))[2]
 for f in folder_files:
-	file_split = os.path.splitext(f)
-	if file_split[-1].lower() in FILETYPES:
-		ctr += 1
-		query = file_split[0]
+    file_split = os.path.splitext(f)
+    if file_split[-1].lower() in FILETYPES:
+        ctr += 1
+        query = file_split[0]
 
 if (ctr > 1) or (not query):
-	# otherwise get the query from cliboard
-	query = Tk().clipboard_get()
+    # otherwise get the query from cliboard
+    query = Tk().clipboard_get()
 
 print "Searching for " + query
 
@@ -57,20 +57,20 @@ soup = BeautifulSoup(html, "html.parser")
 # Parse all subtitle links with an english language tag
 dl_link = ""
 for div in soup.find_all('td', 'a1'):
-	try:
-		link = div.a['href']
-		if "English" in div.find('span', attrs={'class':'l'}).text.strip():
-			url = "http://subscene.com" + str(link)
-			html = urllib2.urlopen(urllib2.Request(url, headers=hdr)).read()
-			soup2 = BeautifulSoup(html, "html.parser")
-			dl_link = soup2.find_all(id='downloadButton')[0]["href"]
-			break;
-	except:
-		continue
-	
+    try:
+        link = div.a['href']
+        if "English" in div.find('span', attrs={'class':'l'}).text.strip():
+            url = "http://subscene.com" + str(link)
+            html = urllib2.urlopen(urllib2.Request(url, headers=hdr)).read()
+            soup2 = BeautifulSoup(html, "html.parser")
+            dl_link = soup2.find_all(id='downloadButton')[0]["href"]
+            break;
+    except:
+        continue
+    
 if not dl_link:
-	print "No subtitles found."
-	sys.exit()
+    print "No subtitles found."
+    sys.exit()
 
 # fetch the download link, and filename from the Header's meta
 url = "http://subscene.com" + str(dl_link)
